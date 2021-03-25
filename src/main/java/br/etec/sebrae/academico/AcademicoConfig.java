f7e01ea4a593301f73d262372fb9b0c15839bbd2
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2021-03-24 22:02:45 -0300

    firt commit

---
+++ src/main/java/br/etec/sebrae/academico/AcademicoConfig.java
@@ -0,0 +1,13 @@
+package br.etec.sebrae.academico;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AcademicoConfig {	
+	@Bean
+	RestTemplate registerTemplate() {
+		return new RestTemplate();
+	}
+}
