68c169c90ac4e3deb18667eefd3e9c1870e669f9
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2021-04-14 22:47:10 -0300

    Criação da tela home com thymelef e bootstrap

--- src/main/java/br/etec/sebrae/academico/controller/HomeController.java
+++ src/main/java/br/etec/sebrae/academico/controller/HomeController.java
@@ -1,58 +1,28 @@
 package br.etec.sebrae.academico.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.Arrays;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
 
-import br.etec.sebrae.academico.dto.Login;
+import br.etec.sebrae.academico.dto.EventoDto;
 
 @Controller
 public class HomeController {
-	
-	final String url = "https://acabemico-sebrae-api.herokuapp.com/login/v1/";
-	RestTemplate rest = new RestTemplate();	
-	
 	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
-	
-	@GetMapping("/lista")
-	public ModelAndView listar() {			
-		ModelAndView modelAndView = new ModelAndView("listalogin");			
-		ArrayList<Login> logins = rest.getForObject(url+"/consultar_login", ArrayList.class);		
-		modelAndView.addObject(new Login());
-		modelAndView.addObject("logins", logins);		
-		return modelAndView;
-	}
-	
-	@PostMapping("/salvar")
-	public String salvar(Login login) {	
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
+	public String index(Model model) {
 		
-		Map<String, Object> map = new HashMap<>();
-		map.put("login", login.getLogin());
-		map.put("tipo", login.getTipo());
-		map.put("nome", login.getNome());
-		map.put("password", login.getPassword());
+		EventoDto evento = new EventoDto();
+		evento.setNomeEvento("Escola Aberta");
+		evento.setDescricao("Semana aberta para a comunidade");
+		LocalDate dataInicio = LocalDate.now();
+		evento.setDataInicio(dataInicio);
+		evento.setDataFim(dataInicio.plusDays(5));
+			
+		model.addAttribute("eventos", Arrays.asList(evento,evento,evento));
 		
-		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map,headers);	
-		
-		ResponseEntity<Login> reponse = rest.postForEntity(url+"/adicionar_login", entity, Login.class);
-		
-		return"redirect:/lista";
-	}
-	
-
+		return "home/index";
+	} 
 }
---
+++ src/main/java/br/etec/sebrae/academico/controller/LoginController.java
@@ -0,0 +1,53 @@
+package br.etec.sebrae.academico.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.etec.sebrae.academico.dto.Login;
+
+@Controller
+public class LoginController {
+	
+	final String url = "https://acabemico-sebrae-api.herokuapp.com/login/v1/";
+	RestTemplate rest = new RestTemplate();	
+	
+	@GetMapping("/lista")
+	public ModelAndView listar() {			
+		ModelAndView modelAndView = new ModelAndView("listalogin");			
+		ArrayList<Login> logins = rest.getForObject(url+"/consultar_login", ArrayList.class);		
+		modelAndView.addObject(new Login());
+		modelAndView.addObject("logins", logins);		
+		return modelAndView;
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Login login) {	
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("login", login.getLogin());
+		map.put("tipo", login.getTipo());
+		map.put("nome", login.getNome());
+		map.put("password", login.getPassword());
+		
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map,headers);	
+		
+		ResponseEntity<Login> reponse = rest.postForEntity(url+"/adicionar_login", entity, Login.class);
+		
+		return"redirect:/lista";
+	}
+	
+
+}
